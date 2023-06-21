214030ce9bd0931e1fc4651dff5679d02ee8b847
Tenzin Norden <nordentenzin29@gmail.com>
2023-06-21 11:10:26 +0530

    base

---
+++ menu-app/src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Launch from "./routes/Launch.tsx";
+import "./styles/index.scss";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Launch />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ menu-app/src/routes/Launch.tsx
@@ -0,0 +1,7 @@
+import "../styles/App.scss";
+
+function Launch() {
+  return <h1>Vite + React</h1>;
+}
+
+export default Launch;
